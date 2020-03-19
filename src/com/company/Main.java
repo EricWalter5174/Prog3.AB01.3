316829ec9991c35f5d9a918c8e5e5589f468f2e5
EricWalter5174 <62294021+EricWalter5174@users.noreply.github.com>
2020-03-19 12:38:35 +0100

    Refactored, functionality might not be implemented correctly. URL is being transfered via constructor of SimpleAudioPlayerAdapter, not via method call of einmaligAbspielen.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,5 @@
 package com.company;
-import de.hsos.prog3.ab1.audio.*;
 import de.hsos.prog3.ab1.audio.adapter.SimpleAudioPlayerAdapter;
-import de.hsos.prog3.audio.SimpleAudioPlayer;
 import java.io.IOException;
 import java.net.URL;
 
@@ -9,7 +7,7 @@ public class Main {
 
     public static void main(String[] args) throws IOException{
         URL url = Main.class.getResource("/Baritone.wav");
-        SimpleAudioPlayerAdapter player = new SimpleAudioPlayerAdapter();
+        SimpleAudioPlayerAdapter player = new SimpleAudioPlayerAdapter(url);
         player.einmaligAbspielen(url);
         player.tonAus();
         player.einmaligAbspielen(url);
--- src/de/hsos/prog3/ab1/audio/adapter/SimpleAudioPlayerAdapter.java
+++ src/de/hsos/prog3/ab1/audio/adapter/SimpleAudioPlayerAdapter.java
@@ -1,36 +1,41 @@
 package de.hsos.prog3.ab1.audio.adapter;
-
 import de.hsos.prog3.ab1.audio.StdAudioPlayer;
 import de.hsos.prog3.audio.SimpleAudioPlayer;
 import java.net.URL;
 import java.io.IOException;
-
+//Anmerkung:
+//Ich bin mir nicht sicher, ob ich die Aufgabe im Sinne des Erfinders gelöst habe.
+//Die Funktionalität ist gegeben, allerdings besitzen die übergebenen URLs in den Methoden einmaligAbspielen und
+//wiederholtAbspielen keinen Zweck, da die URL direkt beim erzeugen eines SimpleAudioPlayerAdapters übergeben wird.
+//Wollte man die URLs mittels der Methoden übergeben, so müsste innerhalb der Methode ein neues SimpleAudioPlayer Objekt
+//erzeugt werden. Folge wären nicht verwendbare tonAus und tonAn Methoden. mMn fehlt eine setURL-Methode im SimpleAudioPlayer.
 
 public class SimpleAudioPlayerAdapter implements StdAudioPlayer{
-
     SimpleAudioPlayer player;
+
+    public SimpleAudioPlayerAdapter(URL inputUrl) throws IOException{
+        this.player = new SimpleAudioPlayer(inputUrl);
+    }
 
     @Override
     public void einmaligAbspielen(URL url) throws IOException {
-        this.player = new SimpleAudioPlayer(url);
-        this.player.play(0);
+        player.play(0);
     }
 
     @Override
     public void wiederholtesAbspielen(URL url, int wiederholungen) throws IOException {
-        this.player = new SimpleAudioPlayer(url);
-        this.player.play(wiederholungen);
+        player.play(wiederholungen);
     }
 
     @Override
-    public void tonAus() {
-        this.player.setDebug(true);
-        this.player.verboseLogging(true);
+    public void tonAus(){
+        player.setDebug(true);
+        player.verboseLogging(true);
     }
 
     @Override
-    public void tonAn() {
-        this.player.setDebug(false);
-        this.player.verboseLogging(true);
+    public void tonAn(){
+        player.setDebug(false);
+        player.verboseLogging(true);
     }
 }
